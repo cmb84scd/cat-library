39f8335b263768978b0776ba2bd4d113d3e7c320
Catriona Bennett <catriona.bennett@dunelm.com>
2023-03-31 15:48:09 +0100

    Refactor tests

--- src/__tests__/musicLibrary.test.ts
+++ src/__tests__/musicLibrary.test.ts
@@ -3,10 +3,6 @@ import { MusicLibrary } from '../musicLibrary'
 describe('Music Library class', () => {
   const musicLibrary = new MusicLibrary()
 
-  beforeEach(() => {
-    musicLibrary.items = []
-  })
-
   it('creates with an empty library', () => {
     expect(musicLibrary.items).toStrictEqual([])
   })
@@ -24,32 +20,32 @@ describe('Music Library class', () => {
     ])
   })
 
-  it('returns the titles of all the items in the library', () => {
-    musicLibrary.add('Country', 'Top band', 'Top country hits', 12, 60)
-    musicLibrary.add('Dance', 'Dance band', 'Best dances', 12, 60)
-    expect(musicLibrary.listByTitles()).toStrictEqual([
-      'Top country hits',
-      'Best dances',
-    ])
-  })
+  describe('Getting data from the music library', () => {
+    const musicLibrary1 = new MusicLibrary()
+    musicLibrary1.add('Country', 'Top band', 'Top country hits', 12, 60)
+    musicLibrary1.add('Country', 'Top band', 'Classic country', 12, 60)
+    musicLibrary1.add('Dance', 'Dance band', 'Best dances', 12, 60)
+    musicLibrary1.add('Jazz', 'JazziB', 'Top jazz hits', 12, 60)
 
-  it('returns all the titles of an artist in the library', () => {
-    musicLibrary.add('Country', 'Top band', 'Top country hits', 12, 60)
-    musicLibrary.add('Country', 'Top band', 'Classic country', 12, 60)
-    musicLibrary.add('Dance', 'Dance band', 'Best dances', 12, 60)
-    musicLibrary.add('Jazz', 'JazziB', 'Top jazz hits', 12, 60)
-    expect(musicLibrary.listByArtist('Top band')).toEqual(
-      'For artist Top band you have the following albums in your library:\n  * Top country hits\n  * Classic country'
-    )
-  })
+    it('returns the titles of all the items in the library', () => {
+      expect(musicLibrary1.listByTitles()).toStrictEqual([
+        'Top country hits',
+        'Classic country',
+        'Best dances',
+        'Top jazz hits',
+      ])
+    })
 
-  it('returns all the titles of an artist in the library', () => {
-    musicLibrary.add('Country', 'Top band', 'Top country hits', 12, 60)
-    musicLibrary.add('Country', 'Top band', 'Classic country', 12, 60)
-    musicLibrary.add('Dance', 'Dance band', 'Best dances', 12, 60)
-    musicLibrary.add('Jazz', 'JazziB', 'Top jazz hits', 12, 60)
-    expect(musicLibrary.listByGenre('Country')).toEqual(
-      'For genre Country you have the following albums in your library:\n  * Top country hits\n  * Classic country'
-    )
+    it('returns all the titles of an artist in the library', () => {
+      expect(musicLibrary1.listByArtist('Top band')).toEqual(
+        'For artist Top band you have the following albums in your library:\n  * Top country hits\n  * Classic country'
+      )
+    })
+
+    it('returns all the titles of an artist in the library', () => {
+      expect(musicLibrary1.listByGenre('Country')).toEqual(
+        'For genre Country you have the following albums in your library:\n  * Top country hits\n  * Classic country'
+      )
+    })
   })
 })
